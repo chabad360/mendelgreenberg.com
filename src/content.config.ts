277cc0d4240e243acea6cdad25b9774c898e3897
Mendel Greenberg <chabad360@gmail.com>
2026-05-05 19:06:41 -0400

    basic page structure is in place

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,22 +1,25 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection } from "astro:content";
 
-// 2. Import loader(s)
-import { glob, file } from "astro/loaders";
+import { glob } from "astro/loaders";
 
-// 3. Import Zod
 import { z } from "astro/zod";
 
-// 4. Define a `loader` and `schema` for each collection
-const blog = defineCollection({
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-  }),
+const posts = defineCollection({
+  loader: glob({ base: "./src/content/posts", pattern: "**/*.{md,mdx}" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.coerce.date(),
+
+      image: image().nullish(),
+      imageAlt: z.string().nullish(),
+
+      publish: z.boolean(),
+
+      tags: z.string().array().optional(),
+      aliases: z.string().array().optional(),
+    }),
 });
 
-// 5. Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { posts };
