5278083da701cee754b2a1fe16bae35ddd6b10f8
Mendel Greenberg <chabad360@gmail.com>
2026-05-06 10:27:25 -0400

    flesh out style

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,30 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { fontProviders } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    "/posts": "/posts/1",
+    "/": "/posts",
+  },
+
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      // name: "IBM Plex Serif",
+      name: "Noto Serif",
+      cssVariable: "--font-serif",
+    },
+    {
+      provider: fontProviders.google(),
+      // name: "Montserrat",
+      name: "Instrument Sans",
+      // name: "Roboto",
+      cssVariable: "--font-sans",
+    },
+  ],
+
+  integrations: [icon()],
+});
